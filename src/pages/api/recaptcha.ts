3b3c2941f9087564ccacb52b8b9d6a97a03c4153
Valentin <valentingaudin@gmail.com>
2024-12-14 21:40:36 +0100

    fix(app): fix modal and contact form

---
+++ src/pages/api/recaptcha.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ResponseData } from '@/types/api';
+
+export default async function handler(
+	request: NextApiRequest,
+	response: NextApiResponse<ResponseData>
+) {
+	if (request.method !== 'POST') {
+		return response.status(405).json({ message: 'Method not allowed' });
+	}
+
+	const data = (await request.body) as { token: string };
+	const { token } = data;
+	const secretKey: string | undefined = process.env.RECAPTCHA_SECRET_KEY;
+
+	if (!token) {
+		return response.status(400).json({ message: 'Token is required' });
+	}
+
+	try {
+		const recaptchaResponse = await fetch(
+			`https://www.google.com/recaptcha/api/siteverify`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: new URLSearchParams({
+					secret: secretKey || '',
+					response: token,
+				}).toString(),
+			}
+		);
+
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const result = await recaptchaResponse.json();
+
+		if (result.success) {
+			return response.status(200).json({ message: 'Successfully verified' });
+		} else {
+			return response.status(400).json({ message: 'Failed to verify' });
+		}
+	} catch (error) {
+		return response.status(500).json({ message: 'An error occurred' });
+	}
+}
