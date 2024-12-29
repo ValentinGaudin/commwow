5a3402302573405d1727acd879f87c732ce72d36
Valentin <valentingaudin@gmail.com>
2024-12-29 18:05:21 +0100

    Your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
 	},
 	env: {
 		APP_RECAPTCHA_SITE_KEY: process.env.APP_RECAPTCHA_SITE_KEY,
-		BREVO_API_KEY: process.env.BREVO_API_KEY
+		BREVO_API_KEY: process.env.BREVO_API_KEY,
 	}
 };
 
