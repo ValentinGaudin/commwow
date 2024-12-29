ad4d0aed15e7dcd4b34420a05d05dc2e5769d509
Valentin <valentingaudin@gmail.com>
2024-12-29 18:01:11 +0100

    build(app): adding env in config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
 	experimental: {
 		esmExternals: true,
 	},
+	env: {
+		APP_RECAPTCHA_SITE_KEY: process.env.APP_RECAPTCHA_SITE_KEY,
+		BREVO_API_KEY: process.env.BREVO_API_KEY,
+	}
 };
 
 export default nextConfig;
